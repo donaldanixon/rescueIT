fe9ccbfee81311663cb3d470d80a5b7e04c903b3
Donald Nixon <donald@nodero.com>
2024-05-22 22:11:41 +1200

    fix up token auth issues

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
   
-    if (!token) return res.sendStatus(401);
+    if (!token || !authHeader) return res.sendStatus(401);
   
     jwt.verify(token, jwtSecret, (err, user) => {
-      if (err) return res.sendStatus(403);
+      if (err) return res.sendStatus(401);
       req.user = user;
       next();
     });
